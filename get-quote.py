4496b23c3831a082b27ca18115dfd5caecc5ebed
liygzting <liygzting@gmail.com>
2022-01-24 14:37:52 +0800

    Read from a file

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,11 @@
 def test():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[0])
 
 if __name__== "__main__":
   test()
